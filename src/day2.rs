ca5e372e0d603cc176266cec112be960b35becac
Ben Montgomery <b3nm0nty@gmail.com>
2025-01-08 16:51:04 -0600

    init day2

---
+++ src/day2.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+use std::error::Error;
+
+pub mod constants {
+    pub const REPORT_FILE_PATH: &str = "day2/reports.txt";
+}
+
+
+pub fn count_safe_reports(file_path: PathBuf) -> Result<u64, Box<dyn Error>> {
+    Ok(0)
+}
